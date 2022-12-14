82d03b271e2649078b2bf3c835cb326731a210f1
Alejandro Soriano <hola@alejandrosoriano.xyz>
2022-12-14 09:37:53 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-pick
- A SketchUp extension to pick random components and/or groups from a selection
+ A SketchUp extension to pick random components and/or groups from a selection.
+
+ Some parts of the code have been generated with ChatGPT artificial intelligence.
---
+++ src/asm_randompick.rb
@@ -0,0 +1,31 @@
+require 'extensions' unless defined?(SketchupExtension)
+
+module ASM_Extensions
+  module RandomPick
+
+    # Constants
+    PLUGIN_NAME = 'Random Pick'.freeze
+    PLUGIN_VERSION = '1.0.0'.freeze
+    PLUGIN_DESCRIPTION = 'Pick random components and/or groups from a selection.'.freeze
+    PLUGIN_AUTHOR = 'Alejandro Soriano'.freeze
+    PLUGIN_ID = File.basename(__FILE__, '.rb')
+
+    # Paths
+    PATH_ROOT = File.dirname(__FILE__)
+    FILE_DATA = File.join(PATH_ROOT, PLUGIN_ID, PLUGIN_ID+"_data.rb")
+    FILE_MAIN = File.join(PATH_ROOT, PLUGIN_ID, PLUGIN_ID+"_main.rb")
+
+    # Create a new instance of the SketchupExtension class
+    EXT_DATA = SketchupExtension.new(PLUGIN_NAME, FILE_MAIN)
+
+    # Attach some nice info
+    EXT_DATA.creator = PLUGIN_AUTHOR
+    EXT_DATA.version = PLUGIN_VERSION
+    EXT_DATA.copyright = "2022-#{Time.now.year}, #{PLUGIN_AUTHOR}"
+    EXT_DATA.description = PLUGIN_DESCRIPTION
+
+    # Register and load the extension on first install
+    Sketchup.register_extension(EXT_DATA, true)
+
+  end # module RandomPick
+end # module ASM
